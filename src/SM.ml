e2541329472d7d6ae2778ee9942bc8b03499196b
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2018-03-05 23:48:21 +0300

    Complete the second assignment

--- src/SM.ml
+++ src/SM.ml
@@ -1,15 +1,15 @@
-open GT       
-       
+open GT
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -17,13 +17,39 @@ type prg = insn list
  *)
 type config = int list * Syntax.Stmt.config
 
+let intToBool x = x != 0
+let boolToInt b = if b then 1 else 0
+
 (* Stack machine interpreter
 
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+let rec eval ((stack, state) as config) prog =
+  let (varState, ins, outs) = state in
+  match prog with
+  | [] -> config
+  | cmd::progTail -> eval (match cmd with
+    | BINOP "+" -> let (y::x::stack) = stack in ((x + y)::stack, state)
+    | BINOP "-" -> let (y::x::stack) = stack in ((x - y)::stack, state)
+    | BINOP "*" -> let (y::x::stack) = stack in ((x * y)::stack, state)
+    | BINOP "/" -> let (y::x::stack) = stack in ((x / y)::stack, state)
+    | BINOP "%" -> let (y::x::stack) = stack in ((x mod y)::stack, state)
+    | BINOP ">" -> let (y::x::stack) = stack in ((boolToInt (x > y))::stack, state)
+    | BINOP "<" -> let (y::x::stack) = stack in ((boolToInt (x < y))::stack, state)
+    | BINOP ">=" -> let (y::x::stack) = stack in ((boolToInt (x >= y))::stack, state)
+    | BINOP "<=" -> let (y::x::stack) = stack in ((boolToInt (x <= y))::stack, state)
+    | BINOP "==" -> let (y::x::stack) = stack in ((boolToInt (x == y))::stack, state)
+    | BINOP "!=" -> let (y::x::stack) = stack in ((boolToInt (x <> y))::stack, state)
+    | BINOP "&&" -> let (y::x::stack) = stack in ((boolToInt (intToBool x && intToBool y))::stack, state)
+    | BINOP "!!" -> let (y::x::stack) = stack in ((boolToInt (intToBool x || intToBool y))::stack, state)
+    | CONST x -> (x::stack, state)
+    | READ -> let (x::ins) = ins in (x::stack, (varState, ins, outs))
+    | WRITE -> let (x::stack) = stack in (stack, (varState, ins, x::outs))
+    | LD var -> ((varState var)::stack, state)
+    | ST var -> let (x::stack) = stack in (stack, ((Syntax.Expr.update var x varState), ins, outs))
+    ) progTail
 
 (* Top-level evaluation
 
@@ -41,4 +67,16 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+let rec compileExprAcc expr acc = match expr with
+  | Syntax.Expr.Const x -> (CONST x)::acc
+  | Syntax.Expr.Var var -> (LD var)::acc
+  | Syntax.Expr.Binop (op, left, right) -> compileExprAcc left @@ compileExprAcc right @@ (BINOP op)::acc
+
+let rec compileAcc stmt acc = match stmt with
+  | Syntax.Stmt.Read var -> READ::(ST var)::acc
+  | Syntax.Stmt.Write (expr) -> compileExprAcc expr (WRITE::acc)
+  | Syntax.Stmt.Assign (var, expr) -> compileExprAcc expr ((ST var)::acc)
+  | Syntax.Stmt.Seq (stmt1, stmt2) -> compileAcc stmt1 (compileAcc stmt2 acc)
+
+let compile stmt = compileAcc stmt []
+
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -1,14 +1,14 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-    
+open GT
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -22,29 +22,49 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
 
+    let boolToInt b = if b then 1 else 0
+    let intToBool x = x != 0
+
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval state expr = match expr with
+      | Const (x) -> x
+      | Var (name) -> state name
+      | Binop (op, left, right) -> match op with
+        | "+" -> eval state left + eval state right
+        | "-" -> eval state left - eval state right
+        | "*" -> eval state left * eval state right
+        | "/" -> eval state left / eval state right
+        | "%" -> eval state left mod eval state right
+        | "==" -> boolToInt (eval state left = eval state right)
+        | "!=" -> boolToInt (eval state left <> eval state right)
+        | "<" -> boolToInt (eval state left < eval state right)
+        | ">" -> boolToInt (eval state left > eval state right)
+        | "<=" -> boolToInt (eval state left <= eval state right)
+        | ">=" -> boolToInt (eval state left >= eval state right)
+        | "&&" -> boolToInt (intToBool (eval state left) && intToBool (eval state right))
+        | "!!" -> boolToInt (intToBool (eval state left) || intToBool (eval state right))
+        | _ -> failwith (Printf.sprintf "Undefined operator %s" op)
 
   end
-                    
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -57,7 +77,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -65,14 +85,20 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
-                                                         
+    let rec eval ((state, ins, outs) as config) stmt = match stmt with
+      | Read (var) -> (match ins with
+        | (x::ins) -> ((Expr.update var x state), ins, outs)
+        | _ -> failwith "Trying to read from an empty input stream")
+      | Write (expr) -> state, ins, (Expr.eval state expr)::outs
+      | Assign (var, expr) -> (Expr.update var (Expr.eval state expr) state), ins, outs
+      | Seq (stmt1, stmt2) -> eval (eval config stmt1) stmt2
+
   end
 
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
-type t = Stmt.t    
+type t = Stmt.t
 
 (* Top-level evaluator
 
