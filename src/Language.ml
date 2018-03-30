1f76463b3d818a5ed4b0049f453167b3166b508d
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2018-03-31 00:54:48 +0300

    Merge in previous homework

--- src/Language.ml
+++ src/Language.ml
@@ -5,13 +5,13 @@ open GT
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
-       
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -25,25 +25,28 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
 
+    let boolToInt b = if b then 1 else 0
+    let intToBool x = x != 0
+
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
-     *)                                                       
+     *)
     let to_func op =
       let bti   = function true -> 1 | _ -> 0 in
       let itb b = b <> 0 in
@@ -62,9 +65,9 @@ module Expr =
       | "!=" -> bti |> (<>)
       | "&&" -> fun x y -> bti (itb x && itb y)
       | "!!" -> fun x y -> bti (itb x || itb y)
-      | _    -> failwith (Printf.sprintf "Unknown binary operator %s" op)    
-    
-    let rec eval st expr =      
+      | _    -> failwith (Printf.sprintf "Unknown binary operator %s" op)
+
+    let rec eval st expr =
       match expr with
       | Const n -> n
       | Var   x -> st x
@@ -74,14 +77,27 @@ module Expr =
 
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string
-                                                                                                                  
+
     *)
-    ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+    ostap (
+      expr:
+        !(Ostap.Util.expr
+          (fun x -> x)
+          [|
+            `Lefta, binopParserList ["!!"];
+            `Lefta, binopParserList ["&&"];
+            `Nona , binopParserList ["!="; "=="; "<="; ">="; "<"; ">"];
+            `Lefta, binopParserList ["+"; "-"];
+            `Lefta, binopParserList ["*"; "/"; "%"];
+          |]
+          primary
+        );
+
+      primary: x:DECIMAL {Const x} | var:IDENT {Var var} | -"(" expr -")"
     )
-    
+
   end
-                    
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -94,7 +110,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -108,18 +124,30 @@ module Stmt =
       | Write   e       -> (st, i, o @ [Expr.eval st e])
       | Assign (x, e)   -> (Expr.update x (Expr.eval st e) st, i, o)
       | Seq    (s1, s2) -> eval (eval conf s1) s2
-                                
+
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not yet implemented"}
+      primary:
+        -"write" -"(" expr:!(Expr.expr) -")" {Write expr} |
+        -"read" -"(" var:IDENT -")" {Read var} |
+        var:IDENT -":=" expr:!(Expr.expr) {Assign (var, expr)};
+
+      stmt:
+        !(Ostap.Util.expr
+          (fun x -> x)
+          [| `Lefta, [ostap (";"), fun left right -> Seq (left, right)] |]
+          primary
+        )
     )
-      
+
+  let parse = stmt
+
   end
 
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
-type t = Stmt.t    
+type t = Stmt.t
 
 (* Top-level evaluator
 
@@ -131,4 +159,4 @@ let eval p i =
   let _, _, o = Stmt.eval (Expr.empty, i, []) p in o
 
 (* Top-level parser *)
-let parse = Stmt.parse                                                     
+let parse = Stmt.parse
